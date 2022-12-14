4aa8221da6cf3332d3cd9359b4c68a868b5393bf
Mahmoud412 <mahmoudashour219@gmail.com>
2022-12-14 16:17:54 +0200

    Updates

---
+++ precache-manifest.371869567fcf96c7f9087b724f0c9b59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ebdc7f2d48f7f1105141",
+    "url": "/phone_app/static/css/main.350cc547.chunk.css"
+  },
+  {
+    "revision": "ebdc7f2d48f7f1105141",
+    "url": "/phone_app/static/js/main.ebdc7f2d.chunk.js"
+  },
+  {
+    "revision": "002a2879119e0b5d31fb",
+    "url": "/phone_app/static/js/1.002a2879.chunk.js"
+  },
+  {
+    "revision": "d05417cccba40b9869f9",
+    "url": "/phone_app/static/js/2.d05417cc.chunk.js"
+  },
+  {
+    "revision": "83c834f24266364baf07",
+    "url": "/phone_app/static/js/runtime~main.83c834f2.js"
+  },
+  {
+    "revision": "2e1871f89af03fe32a31a2bd73d6f154",
+    "url": "/phone_app/index.html"
+  }
+];
